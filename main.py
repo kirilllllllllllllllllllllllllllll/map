65dc4633df8f0443f80848c5e1af400fc8c95825
funcargobull <funcargobull@yahoo.com>
2024-02-08 16:11:29 +0700

    появляется окно

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+import sys
 
+
+class App(QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        uic.loadUi('design.ui', self)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = App()
+    app.exec_()
